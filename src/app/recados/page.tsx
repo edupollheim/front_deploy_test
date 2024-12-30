6a507751168bc7a9a5e6ad6bbb8c1ce96d27380b
Eduardo Anage <eduardo.henrique@anageimoveis.com.br>
2024-12-30 16:04:01 -0300

    alterar ip
    alterar ip

--- src/app/recados/page.tsx
+++ src/app/recados/page.tsx
@@ -16,7 +16,7 @@ type Message = {
     createdAt: string;
 };
 
-const BACKEND_URL = "http://127.0.0.1:3001/api";
+const BACKEND_URL = "http://localhost:3001/api";
 
 const Guestbook = () => {
     const [name, setName] = useState<string>("");
