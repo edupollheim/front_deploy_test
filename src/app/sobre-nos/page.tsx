d37bcd85e2acab274abd5dbf2ac5340a8e92cec7
Eduardo Anage <eduardo.henrique@anageimoveis.com.br>
2025-02-27 21:46:52 -0300

    Alteração da pagina de noivos

--- src/app/sobre-nos/page.tsx
+++ src/app/sobre-nos/page.tsx
@@ -1,56 +1,67 @@
-import React from 'react';
-import noiva from '../images/noiva.webp';
-import noivo from '../images/noivo.webp';
-import casal from '../images/casal.webp';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import casal from "../images/casal.webp";
 import Divider from "@/app/components/divider";
 
-export default function SobreNos() {
-    return (
-        <div className="flex flex-col">
-            {/* Seção dos Noivos */}
-            <section className="flex flex-col items-center justify-center min-h-screen mt-[-5.5rem] p-4 md:p-8 -z-10">
-                <h1 className="text-3xl md:text-4xl font-semibold mb-6 text-[#b6b187]" style={{fontFamily: "Italiana"}}>
-                    Os Noivos
-                </h1>
-                <div className="flex flex-col md:flex-row justify-center gap-6 md:gap-8">
-                    {/* Imagem do Noivo */}
-                    <div className="text-center">
-                        <img
-                            src={noivo.src}
-                            alt="Retrato do Noivo"
-                            className="w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 rounded-full object-cover border-4 shadow-lg border-[#DAA520]"
-                        />
-                        <p className="mt-4 text-lg font-bold text-[#b6b187]" style={{fontFamily: "Italiana"}}>Noivo</p>
-                    </div>
-                    {/* Imagem da Noiva */}
-                    <div className="text-center">
-                        <img
-                            src={noiva.src}
-                            alt="Retrato da Noiva"
-                            className="w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 rounded-full object-cover border-4 border-[#DAA520] shadow-lg"
-                        />
-                        <p className="mt-4 text-lg font-bold text-[#b6b187]" style={{fontFamily: "Italiana"}}>Noiva</p>
-                    </div>
-                </div>
-                {/* Citação */}
-                <div className="w-full px-4 md:px-8 mt-10">
-                    <div className="p-4 rounded-lg">
-                        <p className="text-center text-lg text-[#b6b187]" style={{ fontFamily: 'Italiana' }}>
-                            “Foi o Senhor que fez isto, e é maravilhoso aos nossos olhos” <br /> Salmos 118:23
-                        </p>
-                    </div>
-                </div>
-            </section>
-            {/* Divider */}
-            <Divider />
-            {/* Seção do Casal */}
-            <section className="flex items-center justify-center">
-                <img
-                    src={casal.src}
-                    alt="Casal feliz"
-                    className="inset-0 w-full h-full object-cover"
-                />
-            </section>
-        </div>
-    );
+interface ImageData {
+  id: number;
+  base64: string;
 }
+
+export default function SobreNos() {
+    const [images, setImages] = useState<ImageData[]>([]);
+
+    // Função para buscar as imagens da API
+    useEffect(() => {
+      const fetchImages = async () => {
+        try {
+          const response = await axios.get("http://localhost:3001/api/photos");
+          console.log(response.data); // Confirma se os dados estão corretos
+          setImages(response.data);
+        } catch (error) {
+          console.error("Erro ao buscar imagens:", error);
+        }
+      };
+
+      fetchImages();
+    }, []);
+
+    return (
+      <div className="flex flex-col">
+        {/* Seção dos Noivos */}
+        <section className="flex flex-col items-center justify-center min-h-screen mt-[-5.5rem] p-4 md:p-8 -z-10">
+          <h1
+            className="text-3xl md:text-4xl font-semibold mb-6 text-[#b6b187]"
+            style={{ fontFamily: "Italiana" }}
+          >
+            Os Noivos
+          </h1>
+          <div className="w-full px-4 md:px-8 mt-10">
+            <div className="p-4 rounded-lg">
+              <p
+                className="text-center text-lg text-[#b6b187]"
+                style={{ fontFamily: "Italiana" }}
+              >
+                “Foi o Senhor que fez isto, e é maravilhoso aos nossos olhos” <br />
+                Salmos 118:23
+              </p>
+            </div>
+          </div>
+
+          {/* Seção de Fotos */}
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+            {images.map((image) => (
+              <div key={image.id} className="overflow-hidden rounded-lg shadow-md">
+                <img
+                  src={`data:image/jpeg;base64,${image.base64}`}
+                  alt={`Foto ${image.id}`}
+                  className="w-full h-48 object-cover"
+                />
+              </div>
+            ))}
+          </div>
+        </section>
+    </div>
+    );
+  }
