1f4e7015e5534d43c337396955743dbb72dc08c6
Eduardo Anage <eduardo.henrique@anageimoveis.com.br>
2025-02-27 21:05:21 -0300

    Alteração nas mensgens

--- src/app/components/anuncios.tsx
+++ src/app/components/anuncios.tsx
@@ -9,7 +9,7 @@ const Anuncios = () => {
       },
       anuncio2: {
         title: "Recepção",
-        text: "Depois da cerimônia, vamos celebrar juntos com um delicioso churrasco! Teremos um almoço preparado com todo carinho, seguido por brincadeiras e atividades para manter o clima leve e divertido. Mais tarde, para fechar o dia com chave de ouro, serviremos um lanche da tarde com várias delícias. Venha com energia para aproveitar cada momento conosco!",
+        text: "Depois da cerimônia, teremos um almoço preparado com todo carinho, seguido por brincadeiras e atividades para manter o clima leve e divertido. Venha com energia para aproveitar cada momento conosco!",
       },
     };
   
@@ -17,14 +17,14 @@ const Anuncios = () => {
       <div className="flex justify-between items-center h-screen p-6 bg-[#fffaf6] dark:bg-[#1a1a1a]">
         {/* Coluna de anúncios à esquerda */}
         <div className="flex-1 pr-6 pt-2">
-          <h1 className="text-[3.5rem] font-thin titulo mt-[-4.5rem] text-[#637c8b] text-center">{title}</h1>
+          <h1 className="text-[3.5rem] font-thin titulo mt-[-4.5rem] text-[#d6b293] text-center">{title}</h1>
   
           {/* Renderizando os anúncios */}
           {Object.keys(textos).map((key) => {
             const anuncio = textos[key as keyof typeof textos];
             return (
               <div key={key} className="mt-6 font-letras">
-                <h2 className="text-2xl font-semibold estilo-sub-titulo text-[#637c8b]">{anuncio.title}</h2>
+                <h2 className="text-2xl font-semibold estilo-sub-titulo text-[#d6b293]">{anuncio.title}</h2>
                 <p className="mt-2 text-lg estilo-texto">{anuncio.text}</p>
               </div>
             );
